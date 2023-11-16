78e2312e4bb1ff933f4a34282bb29b884dcb2c32
Jordan-Reeves <jar5927@RIT.edu>
2023-11-16 14:22:06 -0500

    Got OpenAI working
    Figured out how messages work

--- openai-test.js
+++ openai-test.js
@@ -1,14 +1,15 @@
 import OpenAI from "openai";
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+const openai = new OpenAI();
 
 const response = await openai.chat.completions.create({
-  model: "gpt-3.5-turbo",
-  messages: ["Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two-and-a-half times that of all the other planets in the Solar System combined. Jupiter is one of the brightest objects visible to the naked eye in the night sky, and has been known to ancient civilizations since before recorded history. It is named after the Roman god Jupiter.[19] When viewed from Earth, Jupiter can be bright enough for its reflected light to cast visible shadows,[20] and is on average the third-brightest natural object in the night sky after the Moon and Venus."],
-  temperature: 0,
-  max_tokens: 1024,
+    model: "gpt-3.5-turbo",
+    messages:[
+        {"role": "system", "content": "You will be provided with a description of a mood, and your task is to generate the CSS code for a color that matches it. Write your output in json with a single key called 'css_code'."},
+        {"role": "user", "content": "Blue sky at dusk."},
+      ],
+    temperature: 0,
+    max_tokens: 1024,
 });
 
-console.log(response);
+console.log(response.choices[0].message);
