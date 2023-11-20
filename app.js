34b27e256c5569717d7b88a00b588a9becd95e93
nihalkarim <nihalkarim@gmail.com>
2023-11-20 11:02:34 -0500

    wip on the ui

---
+++ app.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const ffmpeg = require("fluent-ffmpeg");
+const mic = require("mic");
+const { Readable } = require("stream");
+const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+
+const { Configuration, OpenAIApi } = require("openai");
